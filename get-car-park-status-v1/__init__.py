e30c2bed4734e52b210709fc32bc73e09a501ad9
sidharthbolar <sidharth.bolar89@gmail.com/>
2021-10-11 11:13:32 +1100

    Separating car park weather api functions

--- get-car-park-status-v1/__init__.py
+++ get-car-park-status-v1/__init__.py
@@ -5,7 +5,7 @@ from shared_code import get_car_par_status,get_weather_status
 from shared_code.get_car_par_status import get_carpark_data
 import requests
 import azure.functions as func
-from shared_code.get_weather_status import get_weather_carpark
+
 
 
 def main(mytimer: func.TimerRequest) -> str:
